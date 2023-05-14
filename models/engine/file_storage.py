188e4b9bc3f66ae9dd1c144dbc8795a2f3245ba8
Feke_21 <favouredubio@gmail.com>
2023-05-14 21:23:44 +0000

    Adding engine and other files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,3 +17,22 @@ class FileStorage:
         """ sets __objects the object with key <obj class name>.id """
         key = obj.__class__.__name__ + '.' + obj.id
         FileStorage.__objects[key] = obj
+
+    def save(self):
+        """ serializes __objects to JSON file(path: __file_path) """
+        copy = FileStorage.__objects
+        obj_dict = {obj: copy[obj].to_dict() for obj in copy.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """ deserializes JSON file only if __file_path exists """
+        try:
+            with open(FileStorage.__file_path) as f:
+                objs = json.load(f)
+                for object in objs.values():
+                    name = object['__class__']
+                    del object['__class__']
+                    self.new(eval(name)(**object))
+        except FileNotFoundError:
+            return
